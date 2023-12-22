29c4197a77fc09d2b579e4c02d9cc317bb26ac94
edao <igd.eda.suparta@gmail.com>
2023-12-22 21:28:55 +0800

    menambahkan data dalam index.txt

--- codetestdemo/main.py
+++ codetestdemo/main.py
@@ -2,7 +2,7 @@ from namecounter import NameCounter
 
 
 def main():
-    namecount = NameCounter("../data/index.txt")
+    namecount = NameCounter("data/index.txt")
     counter = namecount.find_single("I Gede Teguh Satya Dharma", sim_thresh=0.5)
     print(f"Number of occurence: {counter}")
 
